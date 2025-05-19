25e11e674a7009584b10f7349416d7b97d75b987
amit kumar <amitarya95080@gmail.com>
2025-05-19 23:43:53 +0530

    Connect mongodb and get setup of thier

---
+++ backend/config/mongodb.js
@@ -0,0 +1,9 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB=async()=>{
+    mongoose.connection.on('connected',()=>console.log("Database connected"))
+
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/pulse-meet`)
+}
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js' 
 
 
 //app config 
 const app =express()
 const port= process.env.PORT||4000
+connectDB()
 
 //middlewares
 app.use(express.json())
@@ -13,7 +15,7 @@ app.use(cors())
 
 //api endpoints
 app.get('/',(req,res)=>{
-    res.send("API WORKING good")
+    res.send("API WORKING")
 })
 
 app.listen(port,()=>console.log("Server started",port))
