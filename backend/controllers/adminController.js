d47753fdad4612f3666765db366bf45a361d4a8a
amit kumar <amitarya95080@gmail.com>
2025-05-23 22:49:10 +0530

    Add the Authentication and provide secure and safe security to loging in the website

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,7 +3,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
- 
+import jwt from 'jsonwebtoken'
 
 
 // API for adding doctor
@@ -61,5 +61,23 @@ const addDoctor=async(req,res)=>{
         
     }
 }
+// api for the login
+const loginAdmin=async(req,res)=>{
+    try {
+        const {email,password}=req.body
+        if(email===process.env.ADMIN_EMAIL&&password===process.env.ADMIN_PASSWORD){
+            const token=jwt.sign(email+password,process.env.JWT_SECRET) 
+            res.json({success:true,token})
 
-export {addDoctor}
+        }
+        else{
+            res.json({success:false,message:"Invalid credentials"})
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export {addDoctor,loginAdmin}
