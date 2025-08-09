bdc62b92c4b239b2712e35ad4e84c41bfd273cd4
amit kumar <amitarya95080@gmail.com>
2025-08-09 23:55:42 +0530

    implement Dashboard component with context-based data fetching and display

--- admin/src/Pages/Admin/Dashboard.jsx
+++ admin/src/Pages/Admin/Dashboard.jsx
@@ -1,9 +1,50 @@
-    import React from 'react'
+import { useContext } from 'react';
+import { AdminContext } from '../../context/AdminContext';
+import { useEffect } from 'react';
+
     
     const Dashboard = () => {
-      return (
-        <div>Dashboard</div>
+
+      const {aToken,cancelAppointment,dashData,getDashdData} = useContext(AdminContext);
+
+      useEffect(() => {
+        if(aToken){
+          getDashdData();
+        }
+      }, [aToken]);
+
+      return dashData&&(
+        <div className='m-5'>
+
+          <div className='flex flex-wrap gap-3'>
+            <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-200 shadow-md cursor-pointer hover scale-105 transition-all duration-200'>
+              <img src=" " alt="doc_img" />
+              <div>
+                <p>{dashData.doctors}</p>
+                <p>Doctors</p>
+              </div>
+            </div>
+
+            <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-200 shadow-md cursor-pointer hover scale-105 transition-all duration-200'>
+              <img src=" " alt="doc_img" />
+              <div>
+                <p>{dashData.appointments}</p>
+                <p>Appointments</p>
+              </div>
+            </div>
+
+            <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-200 shadow-md cursor-pointer hover scale-105 transition-all duration-200'>
+              <img src=" " alt="doc_img" />
+              <div>
+                <p>{dashData.patients}</p>
+                <p>Patients </p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
       )
     }
     
-    export default Dashboard
+    export default Dashboard;
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import jwt from 'jsonwebtoken'
 import doctorModel from '../models/doctorModel.js'
+import userModel from '../models/userModel.js'
 import appointmentModel from '../models/appointmentModel.js'
 
 
@@ -130,6 +131,27 @@ const appointmentCancel = async (req, res) => {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
-};
+}
 
-export {addDoctor,loginAdmin,allDoctors,appointmentsAdmin, appointmentCancel}
+// API to get dashboard data for admin panel
+
+const adminDashboard=async(req,res)=>{
+    try {
+        const doctors=await doctorModel.find({})
+        const users=await userModel.find({})
+        const appointments=await appointmentModel.find({})
+
+        const dashData={
+            doctors:doctors.length,
+            appointments:appointments.length,
+            patients:users.length,
+            latestAppointments:appointments.reverse().slice(0,5)
+        }
+        res.json({success:true,dashData})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export {addDoctor,loginAdmin,allDoctors,appointmentsAdmin, appointmentCancel,adminDashboard}
